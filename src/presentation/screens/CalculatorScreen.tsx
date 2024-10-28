c31467173d4b36d12588ec3c97c2b976d0fabfb3
ayr77on <ayrtongp77@gmail.com>
2024-10-28 00:41:45 -0300

    Diseño inicial

--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -1,13 +1,45 @@
 
-import { View,Text } from 'react-native'
-import { styles } from '../config/theme/app-theme'
+import { View,Text, Pressable } from 'react-native'
+import { colors, styles } from '../config/theme/app-theme';
+import { CalculatorButton } from '../components/CalculatorButton'
 
 const CalculatorScreen = () => {
   return (
     <View style={styles.calculatorContainer}>
+
       <View style={{paddingHorizontal: 30, paddingBottom:20}}>
         <Text style={styles.mainResult}>1500</Text>
         <Text style={styles.subResult}>15</Text>
+      </View>
+
+      <View style={styles.row}>
+        <CalculatorButton blackText label="C" color={colors.lightGray} />
+        <CalculatorButton blackText label="+/-" color={colors.lightGray} />
+        <CalculatorButton blackText label="del" color={colors.lightGray} />
+        <CalculatorButton label="÷" color={colors.orange}/>
+      </View>
+      <View style={styles.row}>
+        <CalculatorButton label="7"/>
+        <CalculatorButton label="8"/>
+        <CalculatorButton label="9"/>
+        <CalculatorButton label="X" color={colors.orange}/>
+      </View>
+      <View style={styles.row}>
+        <CalculatorButton label="4"/>
+        <CalculatorButton label="5"/>
+        <CalculatorButton label="6"/>
+        <CalculatorButton label="-" color={colors.orange} />
+      </View>
+      <View style={styles.row}>
+        <CalculatorButton label="1"/>
+        <CalculatorButton label="2"/>
+        <CalculatorButton label="3"/>
+        <CalculatorButton label="+" color={colors.orange} />
+      </View>
+      <View style={styles.row}>
+        <CalculatorButton label="0" doubleSize/>
+        <CalculatorButton label="."/>
+        <CalculatorButton label="=" color={colors.orange} />
       </View>
     </View>
   )
